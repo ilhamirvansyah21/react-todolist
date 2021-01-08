fbe46417388e7d1f6b612dbdd415b7313d08efbc
Ilham Irvansyah <70256207+ilhamirvansyah21@users.noreply.github.com>
2021-01-08 09:58:14 +0700

    complete-todo-function

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -7,27 +7,32 @@ import Todos from "../components/Todos";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
-    { text: "Learning Javascript" },
-    { text: "Learning Ionic" },
-    { text: "Learning Raect" }
+    { text: "Learning Javascript", isCompleted: false },
+    { text: "Learning Ionic", isCompleted: false },
+    { text: "Learning Raect", isCompleted: false }
   ]);
 
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
     // [...todos] = Membuat array baru yang ada di state todos
-    const addedTodo = [...todos, { text: value }];
+    const addedTodo = [...todos, { text: value, isCompleted: false }];
 
     setTodos(addedTodo);
   };
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
 
+    setTodos(addedTodo);
+  };
   const showAddToggle = () => setShowAdd(!showAdd);
 
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
