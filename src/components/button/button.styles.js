2a94a5dc2b3cb2d12fcbd10382851f74e3132361
Ilham Irvansyah <70256207+ilhamirvansyah21@users.noreply.github.com>
2021-01-11 11:23:45 +0700

    emotion-css-props-in-button-component

---
+++ src/components/button/button.styles.js
@@ -0,0 +1,31 @@
+import { css } from "@emotion/core";
+
+export const button = ({ color, align }) => {
+  let textColor;
+
+  switch (color) {
+    case "black":
+      textColor = "#484848";
+      break;
+    case "red":
+      textColor = "#e06262";
+      break;
+    default:
+      textColor = "#484848";
+  }
+
+  return css`
+    width: 24%;
+    font-size: 1.8rem;
+    font-family: "Bungee Shade", cursive;
+    color: ${textColor};
+
+    padding: 16px;
+    cursor: pointer;
+
+    background: unset;
+    border: unset;
+    outline: unset;
+    text-align: ${align};
+  `;
+};
