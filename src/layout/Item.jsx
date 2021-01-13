f49ff16738f749e05c0dc8b9e7a9db2351a4c82f
Ilham Irvansyah <70256207+ilhamirvansyah21@users.noreply.github.com>
2021-01-13 13:56:41 +0700

    create-layout-component-item

---
+++ src/layout/Item.jsx
@@ -0,0 +1,26 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import PropTypes from "prop-types";
+
+const Item = ({ children, flex }) => {
+  return (
+    <div
+      className={flex - item}
+      css={css`
+        flex: ${flex};
+      `}
+    >
+      {children}
+    </div>
+  );
+};
+
+Item.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  flex: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
+
+export default Item;
